3274c206b3c29034b4b1afb768563ff945e5e234
Vlad <apukhtin00@inbox.ru>
2025-12-08 14:05:07 +0300

    Added calibrator and data split

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,8 @@
 from src.data_preprocessing import DataPreprocessor
 from src.hyperparameters_tuning import HyperparameterTuner, CatboostHyperparameterTuner
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.calibration import CalibratedClassifierCV
 from config.config import RAW_DATA_DIR, MODELS_DIR
 from src.constants import TARGET_COLUMN
 from catboost import CatBoostClassifier
@@ -14,8 +16,12 @@ class ModelTrainer:
     def train(self, X, y, model_name='catboost', params=None):
         X = self.preprocessor.preprocess_pipeline(X, mode='train')
 
+        X_train, X_val, y_train, y_val = train_test_split(
+            X, y, test_size=0.2, random_state=42, stratify=y
+        )
+
         if params is None:
-            study = self.hyperparameter_tuner.start_tuning(X, y, n_trials=5)
+            study = self.hyperparameter_tuner.start_tuning(X_train, y_train, n_trials=5)
             if model_name=='catboost':
                 model = CatBoostClassifier(**study.best_params)
             else:
@@ -26,24 +32,31 @@ class ModelTrainer:
             else:
                 raise NameError(f"Incorrect model name: '{model_name}'")
 
-        model.fit(X, y, verbose=200)
-        self.save_results(model=model,
-                          preprocessor=self.preprocessor)
-        return model, self.preprocessor
+        model.fit(X_train, y_train, verbose=200)
+        calibrator = CalibratedClassifierCV(model, method="isotonic", cv="prefit")
+        calibrator.fit(X_val, y_val)
 
-    def save_results(self, model, preprocessor):
+        self.save_results(model=model,
+                          preprocessor=self.preprocessor,
+                          calibrated_model=calibrator)
+        return model, self.preprocessor, calibrator
+
+    def save_results(self, model, preprocessor, calibrated_model):
         with open(MODELS_DIR + '/model.pkl', "wb") as f:
             pickle.dump(model, f)
 
         with open(MODELS_DIR + '/preprocessor.pkl', "wb") as f:
             pickle.dump(preprocessor, f)
 
-        print('Model and preprocessor were saved to /models')
+        with open(MODELS_DIR + '/calibrated_model.pkl', "wb") as f:
+            pickle.dump(calibrated_model, f)
+
+        print('Model, calibrated model, and preprocessor were saved to /models')
 
 if __name__ == "__main__":
-    data = pd.read_csv(RAW_DATA_DIR + '/train.csv').iloc[:200]
+    data = pd.read_csv(RAW_DATA_DIR + '/train.csv').iloc[:3000]
     trainer = ModelTrainer(DataPreprocessor(), CatboostHyperparameterTuner())
     X = data.drop(TARGET_COLUMN, axis=1)
     y = data[TARGET_COLUMN]
-    model, preprocessor = trainer.train(X, y, model_name='catboost')
+    model, preprocessor, calibrated_model = trainer.train(X, y, model_name='catboost')
     print(model, preprocessor)
